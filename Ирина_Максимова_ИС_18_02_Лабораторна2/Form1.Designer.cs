d19372c767a119a1ce41d23f4e95761ea1d731a3
iramaksimova <urivams31@outlook.com>
2020-04-11 21:01:17 +0300

    Добавьте файлы проекта.

---
+++ Ирина_Максимова_ИС_18_02_Лабораторна2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Ирина_Максимова_ИС_18_02_Лабораторна2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RASSCHITAT = new System.Windows.Forms.Button();
+            this.Konvert2 = new System.Windows.Forms.ComboBox();
+            this.Konvert1 = new System.Windows.Forms.ComboBox();
+            this.Number = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // RASSCHITAT
+            // 
+            this.RASSCHITAT.Location = new System.Drawing.Point(329, 348);
+            this.RASSCHITAT.Name = "RASSCHITAT";
+            this.RASSCHITAT.Size = new System.Drawing.Size(143, 37);
+            this.RASSCHITAT.TabIndex = 0;
+            this.RASSCHITAT.Text = "Рассчитать";
+            this.RASSCHITAT.UseVisualStyleBackColor = true;
+            this.RASSCHITAT.Click += new System.EventHandler(this.RASSCHITAT_Click);
+            // 
+            // Konvert2
+            // 
+            this.Konvert2.FormattingEnabled = true;
+            this.Konvert2.Items.AddRange(new object[] {
+            "Квадратный Милиметр(ММ)",
+            "Квадратный Сантиметр(СМ)",
+            "Квадратный Дециметр(ДМ)",
+            "Квадратный метр(М)",
+            "Аршин(А)",
+            "Гектар(ГА)",
+            "Километр(КМ)"});
+            this.Konvert2.Location = new System.Drawing.Point(560, 178);
+            this.Konvert2.Name = "Konvert2";
+            this.Konvert2.Size = new System.Drawing.Size(193, 21);
+            this.Konvert2.TabIndex = 1;
+            // 
+            // Konvert1
+            // 
+            this.Konvert1.FormattingEnabled = true;
+            this.Konvert1.Items.AddRange(new object[] {
+            "Квадратный Милиметр(ММ)",
+            "Квадратный Сантиметр(СМ)",
+            "Квадратный Дециметр(ДМ)",
+            "Квадратный метр(М)",
+            "Аршин(А)",
+            "Гектар(ГА)",
+            "Километр(КМ)"});
+            this.Konvert1.Location = new System.Drawing.Point(287, 178);
+            this.Konvert1.Name = "Konvert1";
+            this.Konvert1.Size = new System.Drawing.Size(185, 21);
+            this.Konvert1.TabIndex = 2;
+            // 
+            // Number
+            // 
+            this.Number.Location = new System.Drawing.Point(99, 178);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(100, 20);
+            this.Number.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.Konvert1);
+            this.Controls.Add(this.Konvert2);
+            this.Controls.Add(this.RASSCHITAT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RASSCHITAT;
+        private System.Windows.Forms.ComboBox Konvert2;
+        private System.Windows.Forms.ComboBox Konvert1;
+        private System.Windows.Forms.TextBox Number;
+    }
+}
+
---
+++ Ирина_Максимова_ИС_18_02_Лабораторна2/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ирина_Максимова_ИС_18_02_Лабораторна2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RASSCHITAT_Click(object sender, EventArgs e)
+        {
+            double Edin = Convert.ToDouble(Number.Text);
+
+
+            switch (Konvert1.SelectedIndex)
+            {
+                case 0:
+                    if (Konvert2.SelectedIndex == 0)
+                    {
+
+                        MessageBox.Show(Edin.ToString() + " MM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 1)
+                    {
+                        double PHEP = Edin / 100;
+
+                        MessageBox.Show(PHEP.ToString() + " CM");
+                    }
+                    if (Konvert2.SelectedIndex == 2)
+                    {
+                        double HEp = Edin / 10000;
+                        MessageBox.Show(HEp.ToString() + " DM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 3)
+                    {
+                        double NET = Edin / 1000000;
+                        MessageBox.Show(NET.ToString() + " M");
+                    }
+                    if (Konvert2.SelectedIndex == 4)
+                    {
+                        double pimp = Edin / 100000000;
+                        MessageBox.Show(pimp.ToString() + " A");
+                    }
+
+                    if (Konvert2.SelectedIndex == 5)
+                    {
+                        double MEG = Edin / 10000000000;
+                        MessageBox.Show(MEG.ToString() + " ГА");
+                    }
+                    if (Konvert2.SelectedIndex == 6)
+                    {
+                        double Drag = Edin / 1000000000000;
+                        MessageBox.Show(Drag.ToString() + " KM");
+                    }
+
+
+
+
+                    break;
+                case 1:
+                    if (Konvert2.SelectedIndex == 0)
+                    {
+                        double X = Edin * 100;
+
+                        MessageBox.Show(X.ToString() + " MM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 1)
+                    {
+
+
+                        MessageBox.Show(Edin.ToString());
+                    }
+                    if (Konvert2.SelectedIndex == 2)
+                    {
+                        double HEp = Edin / 100;
+                        MessageBox.Show(HEp.ToString() + " DM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 3)
+                    {
+                        double NET = Edin / 10000;
+                        MessageBox.Show(NET.ToString() + " M");
+                    }
+                    if (Konvert2.SelectedIndex == 4)
+                    {
+                        double pimp = Edin / 1000000;
+                        MessageBox.Show(pimp.ToString() + " A");
+                    }
+
+                    if (Konvert2.SelectedIndex == 5)
+                    {
+                        double MEG = Edin / 100000000;
+                        MessageBox.Show(MEG.ToString() + " ГА");
+                    }
+                    if (Konvert2.SelectedIndex == 6)
+                    {
+                        double Drag = Edin / 10000000000;
+                        MessageBox.Show(Drag.ToString() + " KM");
+                    }
+
+
+
+
+                    break;
+                case 2:
+                    if (Konvert2.SelectedIndex == 0)
+                    {
+                        double X = Edin * 10000;
+
+                        MessageBox.Show(Edin.ToString() + " MM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 1)
+                    {
+                        double PHEP = Edin * 100;
+
+                        MessageBox.Show(PHEP.ToString() + " CM");
+                    }
+                    if (Konvert2.SelectedIndex == 2)
+                    {
+
+                        MessageBox.Show(Edin.ToString() + "DM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 3)
+                    {
+                        double NET = Edin / 100;
+                        MessageBox.Show(NET.ToString() + " M");
+                    }
+                    if (Konvert2.SelectedIndex == 4)
+                    {
+                        double pimp = Edin / 10000;
+                        MessageBox.Show(pimp.ToString() + " A");
+                    }
+
+                    if (Konvert2.SelectedIndex == 5)
+                    {
+                        double MEG = Edin / 1000000;
+                        MessageBox.Show(MEG.ToString() + " ГА");
+                    }
+                    if (Konvert2.SelectedIndex == 6)
+                    {
+                        double Drag = Edin / 100000000;
+                        MessageBox.Show(Drag.ToString() + " KM");
+                    }
+
+
+
+
+                    break;
+
+                case 3:
+                    if (Konvert2.SelectedIndex == 0)
+                    {
+                        double X = Edin * 1000000;
+                        MessageBox.Show(X.ToString() + " MM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 1)
+                    {
+                        double PHEP = Edin * 10000;
+
+                        MessageBox.Show(PHEP.ToString() + " CM");
+                    }
+                    if (Konvert2.SelectedIndex == 2)
+                    {
+                        double HEp = Edin * 100;
+                        MessageBox.Show(HEp.ToString() + " DM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 3)
+                    {
+
+                        MessageBox.Show(Edin.ToString() + " M");
+                    }
+                    if (Konvert2.SelectedIndex == 4)
+                    {
+                        double pimp = Edin / 100;
+                        MessageBox.Show(pimp.ToString() + " A");
+                    }
+
+                    if (Konvert2.SelectedIndex == 5)
+                    {
+                        double MEG = Edin / 10000;
+                        MessageBox.Show(MEG.ToString() + " ГА");
+                    }
+                    if (Konvert2.SelectedIndex == 6)
+                    {
+                        double Drag = Edin / 1000000;
+                        MessageBox.Show(Drag.ToString() + " KM");
+                    }
+
+
+
+
+                    break;
+
+                case 4:
+                    if (Konvert2.SelectedIndex == 0)
+                    {
+                        double X = Edin * 100000000;
+                        MessageBox.Show(X.ToString() + " MM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 1)
+                    {
+                        double PHEP = Edin * 1000000;
+
+                        MessageBox.Show(PHEP.ToString() + " CM");
+                    }
+                    if (Konvert2.SelectedIndex == 2)
+                    {
+                        double HEp = Edin * 10000;
+                        MessageBox.Show(HEp.ToString() + " DM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 3)
+                    {
+                        double NET = Edin * 100;
+                        MessageBox.Show(NET.ToString() + " M");
+                    }
+                    if (Konvert2.SelectedIndex == 4)
+                    {
+
+                        MessageBox.Show(Edin.ToString() + " A");
+                    }
+
+                    if (Konvert2.SelectedIndex == 5)
+                    {
+                        double MEG = Edin / 100;
+
+                        MessageBox.Show(MEG.ToString() + " ГА");
+                    }
+                    if (Konvert2.SelectedIndex == 6)
+                    {
+                        double Drag = Edin / 10000;
+                        MessageBox.Show(Drag.ToString() + " KM");
+                    }
+
+
+
+
+                    break;
+
+                case 5:
+                    if (Konvert2.SelectedIndex == 0)
+                    {
+                        double X = Edin * 10000000000;
+
+                        MessageBox.Show(X.ToString() + " MM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 1)
+                    {
+                        double PHEP = Edin * 100000000;
+
+                        MessageBox.Show(PHEP.ToString() + " CM");
+                    }
+                    if (Konvert2.SelectedIndex == 2)
+                    {
+                        double HEp = Edin * 1000000;
+                        MessageBox.Show(HEp.ToString() + " DM");
+                    }
+
+                    if (Konvert2.SelectedIndex == 3)
+                    {
+                        double NET = Edin * 10000;
+                        MessageBox.Show(NET.ToString() + " M");
+                    }
+                    if (Konvert2.SelectedIndex == 4)
+                    {
+                        double pimp = Edin * 100;
+                        MessageBox.Show(pimp.ToString() + " A");
+                    }
+
+                    if (Konvert2.SelectedIndex == 5)
+                    {
+                        MessageBox.Show(Edin.ToString() + " ГА");
+                    }
+                    if (Konvert2.SelectedIndex == 6)
+                    {
+                        double Drag = Edin * 100;
+                        MessageBox.Show(Drag.ToString() + " KM");
+                    }
+
+
+
+
+                    break;
+            }
+
+        }
+
+        private void Konvert1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+        
+    
+
